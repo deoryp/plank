d9cc7c7fcc4073a094eb03525f6bfdd8b5a825e9
Scott Nichols <snichols@riotgames.com>
2015-07-12 13:59:52 -0700

    updated the src

--- release/bundles/app/Client.js
+++ release/bundles/app/Client.js
@@ -327,9 +327,6 @@ angular.module('plankApp')
     
     
     $scope.createNewThread = ThreadModal.create.thread(function (event, args) {
-      console.log('called back from create thread');
-      console.log(this.results);
-      
       // TODO:: check the results are valid.
       
       $http.post('/api/thread/' + forum + '/', {
